49f1f5a26e860a9850dfe5a4d6ba4bb6e309315f
Krystian Madra <madrakrystian@gmail.com>
2021-09-21 20:36:44 +0200

    Narrow down context to files within test module

--- src/main/java/com/madrakrystian/juneed/template/live/JavaTestingContext.java
+++ src/main/java/com/madrakrystian/juneed/template/live/JavaTestingContext.java
@@ -2,13 +2,15 @@ package com.madrakrystian.juneed.template.live;
 
 import com.intellij.codeInsight.template.TemplateActionContext;
 import com.intellij.codeInsight.template.TemplateContextType;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
 public class JavaTestingContext extends TemplateContextType {
 
-    public static final String CONTEXT_ID = "JAVA_TESTING";
-    public static final String CONTEXT_PRESENTABLE_NAME = "Java testing";
+    private static final String CONTEXT_ID = "JAVA_TESTING";
+    private static final String CONTEXT_PRESENTABLE_NAME = "Java testing";
 
     protected JavaTestingContext() {
         super(CONTEXT_ID, CONTEXT_PRESENTABLE_NAME);
@@ -18,7 +20,19 @@ public class JavaTestingContext extends TemplateContextType {
     public boolean isInContext(@NotNull TemplateActionContext templateActionContext) {
         final PsiFile contextFile = templateActionContext.getFile();
 
-        // todo check if within test directory
+        return isAJavaTest(contextFile) && isWithinTestModule(contextFile.getOriginalFile());
+    }
+
+    private boolean isAJavaTest(PsiFile contextFile) {
         return contextFile.getName().endsWith("Test.java");
+    }
+
+    private boolean isWithinTestModule(@NotNull PsiFile originalFile) {
+        final Module module = ModuleUtil.findModuleForFile(originalFile);
+
+        if (module != null) {
+            return module.getName().endsWith("test");
+        }
+        return false;
     }
 }
