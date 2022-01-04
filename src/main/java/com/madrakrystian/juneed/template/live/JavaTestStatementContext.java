e0681af1a47c534fbfd2d12dc7a986776d5c6fe9
Krystian Madra <madrakrystian@gmail.com>
2022-01-04 22:15:15 +0100

    Correct java test statement context readability, add some javadocs

--- src/main/java/com/madrakrystian/juneed/template/live/JavaTestStatementContext.java
+++ src/main/java/com/madrakrystian/juneed/template/live/JavaTestStatementContext.java
@@ -5,17 +5,28 @@ import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
-import static com.madrakrystian.juneed.template.live.JavaTestFileValidator.*;
+import static com.madrakrystian.juneed.template.live.JavaTestSourcePredicates.HAS_TEST_SUFFIX;
+import static com.madrakrystian.juneed.template.live.JavaTestSourcePredicates.IS_WITHIN_TEST_SOURCES;
 
+/**
+ * Provides context of java test statement.
+ * Can be used in the plugin.xml to provide liveTemplateContext implementation
+ */
 public class JavaTestStatementContext extends JavaCodeContextType.Statement {
 
     private static final String CONTEXT_ID = "JAVA_TEST_STATEMENT";
     private static final String CONTEXT_PRESENTABLE_NAME = "Java test statement";
 
+    /**
+     * Checks if given {@link PsiElement} is in java test statement context.
+     *
+     * @param element part of the PSI tree e.g. Java File
+     * @return true if given element fulfills all statement and java tests conditions, false otherwise
+     */
     @Override
     protected boolean isInContext(@NotNull PsiElement element) {
-        return super.isInContext(element) // checks if element is inside java statement
-                && isJavaTest(element.getContainingFile().getOriginalFile());
+        return super.isInContext(element) &&
+                HAS_TEST_SUFFIX.and(IS_WITHIN_TEST_SOURCES).test(element.getContainingFile().getOriginalFile());
     }
 
     @Override
