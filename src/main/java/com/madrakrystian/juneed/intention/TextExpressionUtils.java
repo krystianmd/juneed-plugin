47ac6a681b9182225dfeb9edd42b11b5c06c4f63
krystianmd <madrakrystian@gmail.com>
2022-02-15 21:24:54 +0100

    Add helping class for expression text operations

---
+++ src/main/java/com/madrakrystian/juneed/intention/TextExpressionUtils.java
@@ -0,0 +1,8 @@
+package com.madrakrystian.juneed.intention;
+
+final class TextExpressionUtils {
+
+    static String expressionLine(String expression, String parameter) {
+        return expression + "(" + parameter + ")";
+    }
+}
