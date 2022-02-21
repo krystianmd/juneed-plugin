ab7d4b38c224a763ea79d08e9c77a8715a82bcaf
krystianmd <madrakrystian@gmail.com>
2022-02-21 20:33:59 +0100

    Change formatter to psi method call expression factory

---
+++ src/main/java/com/madrakrystian/juneed/psi/PsiMethodCallExpressionFactory.java
@@ -0,0 +1,25 @@
+package com.madrakrystian.juneed.psi;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiMethodCallExpression;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.psi.codeStyle.JavaCodeStyleManager;
+import org.jetbrains.annotations.NotNull;
+
+public final class PsiMethodCallExpressionFactory {
+
+    /**
+     * Creates assertion {@link PsiMethodCallExpression} from expression text and formats it according to the the {@link Project} style.
+     */
+    public static @NotNull PsiMethodCallExpression createFormatted(@NotNull Project project, @NotNull String expressionText) {
+        final PsiElementFactory factory = JavaPsiFacade.getInstance(project).getElementFactory();
+        PsiMethodCallExpression assertion =  (PsiMethodCallExpression) factory
+                .createExpressionFromText(expressionText, null);
+
+        final CodeStyleManager codeStylist = CodeStyleManager.getInstance(project);
+        return (PsiMethodCallExpression) codeStylist
+                .reformat(JavaCodeStyleManager.getInstance(project).shortenClassReferences(assertion));
+    }
+}
