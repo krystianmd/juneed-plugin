81f64846acb8ced352694f30986bfdd416a27912
krystianmd <madrakrystian@gmail.com>
2022-03-23 20:00:25 +0100

    Divide assertion factory responsibilities to separate methods

--- src/main/java/com/madrakrystian/juneed/utils/methodCall/AssertionFactory.java
+++ src/main/java/com/madrakrystian/juneed/utils/methodCall/AssertionFactory.java
@@ -14,10 +14,17 @@ public final class AssertionFactory {
      * Creates assertion {@link PsiMethodCallExpression} from expression text and formats it according to the the {@link Project} style.
      */
     public static @NotNull PsiMethodCallExpression create(@NotNull Project project, @NotNull String expressionText) {
-        final PsiElementFactory factory = JavaPsiFacade.getInstance(project).getElementFactory();
-        final PsiMethodCallExpression assertion = (PsiMethodCallExpression) factory
-                .createExpressionFromText(expressionText, null);
+        return formatAssertion(project, createAssertion(project, expressionText));
+    }
 
+    @NotNull
+    private static PsiMethodCallExpression createAssertion(@NotNull Project project, @NotNull String expressionText) {
+        final PsiElementFactory factory = JavaPsiFacade.getInstance(project).getElementFactory();
+        return (PsiMethodCallExpression) factory.createExpressionFromText(expressionText, null);
+    }
+
+    @NotNull
+    private static PsiMethodCallExpression formatAssertion(@NotNull Project project, @NotNull PsiMethodCallExpression assertion) {
         final CodeStyleManager codeStylist = CodeStyleManager.getInstance(project);
         return (PsiMethodCallExpression) codeStylist
                 .reformat(JavaCodeStyleManager.getInstance(project).shortenClassReferences(assertion));
